25ca817a839df28322ea0b42d954560dfbf89a38
Devanggairola7 <devanggairola687@gmail.com>
2025-10-26 14:37:57 +0530

    Update check a number.c

--- if else/check a number.c
+++ if else/check a number.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{int number;
+ printf("Enter a number ");
+ scanf("%d",&number);
+ if(number>0)
+  printf("the number is positive.");
+ else if(number<0)
+  printf("The number is negative.");
+ else
+  printf("The number is zero.");
+ printf("\n");
+ if(number%2==0)
+  printf("The number is even.");
+ else
+  printf("The number is odd.");
 
+return 0;
+}
