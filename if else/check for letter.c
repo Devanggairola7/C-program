1567522af989470cc9e6b26a4783871c62f3dae8
Devanggairola7 <devanggairola687@gmail.com>
2025-10-27 21:00:50 +0530

    Update check for letter.c

--- if else/check for letter.c
+++ if else/check for letter.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
+#include<ctype.h>
 int main()
 {char letter;
  printf("Enter a letter to be checked for vowel or consonant. ");
  scanf("%c",&letter);
- if(letter=='a')
- printf("Vowel");
- else if(letter=='e')
- printf("Vowel");
- else if(letter=='i')
- printf("Vowel");
- else if(letter=='o')
- printf("Vowel");
- else if(letter=='u')
- printf("Vowel");
- else 
- printf("Consonant");
+ {if(isalpha(letter))
+  {if(letter=='a')
+   printf("Vowel");
+   else if(letter=='e')
+   printf("Vowel");
+   else if(letter=='i')
+   printf("Vowel");
+   else if(letter=='o')
+   printf("Vowel");
+   else if(letter=='u')
+   printf("Vowel");
+   else 
+   printf("Consonant");
+ }
+ else
+ printf("Not an alphabet");
+ }
 return 0;
 }
